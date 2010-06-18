e835ae330f2fd9dad4a3624c5919a388b7f27866
Russ Allbery <rra@stanford.edu>
2010-06-18 11:30:15 -0700

    Flesh out the pam-util logging tests
    Also test the PAM and Kerberos error reporting functions.

--- tests/pam-util/logging-t.c
+++ tests/pam-util/logging-t.c
@@ -30,7 +30,33 @@
         is_string(expected, seen, "%s", (n));         \
         free(seen);                                   \
         free(expected);                               \
-    } while(0);
+    } while (0);
+
+/* Test a PAM error logging function. */
+#define TEST_PAM(func, c, p, n)                         \
+    do {                                                \
+        (func)(args, (c), "%s", "bar");                 \
+        asprintf(&expected, "%d %s: %s", (p), "bar",    \
+                 pam_strerror(args->pamh, c));          \
+        seen = pam_output();                            \
+        is_string(expected, seen, "%s", (n));           \
+        free(seen);                                     \
+        free(expected);                                 \
+    } while (0);
+
+/* Test a PAM Kerberos error logging function .*/
+#define TEST_KRB5(func, p, n)                                             \
+    do {                                                                  \
+        code = krb5_parse_name(args->ctx, "foo@bar@EXAMPLE.COM", &princ); \
+        (func)(args, code, "%s", "krb");                                  \
+        code = krb5_parse_name(args->ctx, "foo@bar@EXAMPLE.COM", &princ); \
+        asprintf(&expected, "%d %s: %s", (p), "krb",                      \
+                 krb5_get_error_message(args->ctx, code));                \
+        seen = pam_output();                                              \
+        is_string(expected, seen, "%s", (n));                             \
+        free(seen);                                                       \
+        free(expected);                                                   \
+    } while (0);
 
 
 int
@@ -39,8 +65,12 @@ main(void)
     pam_handle_t *pamh;
     struct pam_args *args;
     char *expected, *seen;
+#ifdef HAVE_KERBEROS
+    krb5_error_code code;
+    krb5_principal princ;
+#endif
 
-    plan(4);
+    plan(13);
 
     if (pam_start(NULL, NULL, NULL, &pamh) != PAM_SUCCESS)
         sysbail("Fake PAM initialization failed");
@@ -51,6 +81,29 @@ main(void)
     ok(pam_output() == NULL, "putil_debug without debug on");
     args->debug = true;
     TEST(putil_debug, LOG_DEBUG, "putil_debug");
+    args->debug = false;
+
+    TEST_PAM(putil_crit_pam,  PAM_SYSTEM_ERR, LOG_CRIT,  "putil_crit_pam S");
+    TEST_PAM(putil_crit_pam,  PAM_BUF_ERR,    LOG_CRIT,  "putil_crit_pam B");
+    TEST_PAM(putil_err_pam,   PAM_SYSTEM_ERR, LOG_ERR,   "putil_err_pam");
+    putil_debug_pam(args, PAM_SYSTEM_ERR, "%s", "bar");
+    ok(pam_output() == NULL, "putil_debug_pam without debug on");
+    args->debug = true;
+    TEST_PAM(putil_debug_pam, PAM_SYSTEM_ERR, LOG_DEBUG, "putil_debug_pam");
+    args->debug = false;
+
+#ifdef HAVE_KERBEROS
+    TEST_KRB5(putil_crit_krb5,  LOG_CRIT,  "putil_crit_krb5");
+    TEST_KRB5(putil_err_krb5,   LOG_ERR,   "putil_err_krb5");
+    code = krb5_parse_name(args->ctx, "foo@bar@EXAMPLE.COM", &princ);
+    putil_debug_krb5(args, code, "%s", "krb");
+    ok(pam_output() == NULL, "putil_debug_krb5 without debug on");
+    args->debug = true;
+    TEST_KRB5(putil_debug_krb5, LOG_DEBUG, "putil_debug_krb5");
+    args->debug = false;
+#else
+    skip_block(4, "not built with Kerberos support");
+#endif
 
     return 0;
 }
