acd6faf412e4060fe7c1fa5d6a3c3733a4d975a1
Russ Allbery <rra@stanford.edu>
2010-07-01 15:53:16 -0700

    Significantly improve basic module test framework
    Build out a much better testing framework for module operations
    and use it to test all PAM module actions when the module is
    configured to do nothing.

--- tests/module/basic-t.c
+++ tests/module/basic-t.c
@@ -17,65 +17,140 @@
 #include <tests/fakepam/testing.h>
 #include <tests/tap/basic.h>
 
+/*
+ * These macros implement a bit of sneakiness.  For all of our argv strings,
+ * debug is the last argument.  Based on the debug variable setting, choose
+ * whether to include that argument or not.
+ */
 #define ARRAY_SIZEOF(a) (sizeof(a) / sizeof((a)[0]))
+#define ARGV(a) ARRAY_SIZEOF(a) - (debug ? 1 : 2), (a)
 
 
-int
-main(void)
+/*
+ * Make a call to a PAM module function and then check it with is_pam_call.
+ * Takes the PAM method, the flags, the argv to use, the expected output, the
+ * expected status, and a string description.
+ */
+#define TEST_PAM(m, f, a, o, s, d)                                      \
+    do {                                                                \
+        is_pam_call((o), (s), m(pamh, (f), ARGV(a)), #m, (f), debug,    \
+                    #m " %s%s", (d), debug_desc);                       \
+    } while (0)
+
+
+/*
+ * Checks a PAM call, taking its return status, the expected logging output
+ * and exit status, the function name, the flags passed into the call, a
+ * boolean to say whether debug will be enabled (which affects the logging
+ * output), and a string to use for the test description.
+ */
+static void
+is_pam_call(const char *output, int expected, int seen, const char *function,
+            int flags, bool debug, const char *format, ...)
+{
+    char *logs, *desc;
+    char *p = NULL;
+    va_list args;
+
+    va_start(args, format);
+    if (vasprintf(&desc, format, args) < 0)
+        sysbail("cannot allocate memory");
+    va_end(args);
+    logs = pam_output();
+    if (!k_hasafs()) {
+        is_int(PAM_IGNORE, seen, "%s (status)", desc);
+        if (debug) {
+            if (asprintf(&p, "%d %s: entry (0x%x)"
+                         "%d skipping, AFS apparently not available"
+                         "%d %s: exit (ignore)", LOG_DEBUG, function,
+                         flags, LOG_ERR, LOG_DEBUG, function) < 0)
+                sysbail("cannot allocate memory");
+        }
+        is_string(p, logs, "%s (output)", desc);
+        if (p != NULL)
+            free(p);
+    } else {
+        is_int(expected, seen, "%s (status)", desc);
+        if (debug) {
+            if (asprintf(&p, "%d %s: entry (0x%x)%s%d %s: exit (%s)",
+                         LOG_DEBUG, function, flags, output, LOG_DEBUG,
+                         function, ((expected == PAM_SUCCESS) ? "success"
+                                    : (expected == PAM_IGNORE) ? "ignore"
+                                    : "failure")) < 0)
+                sysbail("cannot allocate memory");
+            is_string(p, logs, "%s (output)", desc);
+        } else {
+            if (output != NULL && output[0] == '\0')
+                is_string(NULL, logs, "%s (output)", desc);
+            else
+                is_string(output, logs, "%s (output)", desc);
+        }
+        if (p != NULL)
+            free(p);
+    }
+    if (logs != NULL)
+        free(logs);
+    free(desc);
+}
+
+
+/*
+ * We run the entire test suite twice, once with debug disabled and once with
+ * debug enabled.  This is the wrapper around all the test cases to enable
+ * that without code duplication.
+ */
+static void
+run_tests(bool debug)
 {
     pam_handle_t *pamh;
     int status;
-    char *output, *expected;
+    char *skipping;
     struct pam_conv conv = { NULL, NULL };
-    const char *argv_nothing[] = { "nopag", "notokens", NULL };
-    const char *argv_nothing_debug[] = { "nopag", "notokens", "debug", NULL };
+    const char *debug_desc = debug ? " w/debug" : "";
+    const char *argv_nothing[] = { "nopag", "notokens", "debug", NULL };
 
-    plan(4);
+    /* Build some messages that we'll use multiple times. */
+    if (asprintf(&skipping, "%d skipping as configured", LOG_DEBUG) < 0)
+        sysbail("cannot allocate memory");
 
     /* Do nothing and check for correct output status. */
     status = pam_start("test", "testuser", &conv, &pamh);
     if (status != PAM_SUCCESS)
         sysbail("cannot create PAM handle");
-    status = pam_sm_open_session(pamh, 0, ARRAY_SIZEOF(argv_nothing) - 1,
-                                 argv_nothing);
-    output = pam_output();
-    if (!k_hasafs()) {
-        is_int(status, PAM_IGNORE, "session status, no AFS");
-        asprintf(&expected, "%d pam_sm_open_session: entry (0x0)", LOG_ERR);
-        is_string(expected, output, "session output, no AFS");
-        free(expected);
-    } else {
-        is_int(status, PAM_SUCCESS, "session status, do nothing");
-        is_string(NULL, output, "session output, do nothing");
-    }
-    free(output);
+    status = pam_sm_authenticate(pamh, 0, ARGV(argv_nothing));
+    is_int(PAM_SUCCESS, status, "auth do nothing%s (status)", debug_desc);
+    ok(pam_output() == NULL, "auth do nothing%s (output)", debug_desc);
+    TEST_PAM(pam_sm_setcred, 0, argv_nothing,
+             "", PAM_SUCCESS,
+             "do nothing");
+    TEST_PAM(pam_sm_setcred, PAM_DELETE_CRED, argv_nothing,
+             (debug ? skipping : ""), PAM_IGNORE,
+             "delete do nothing");
+    TEST_PAM(pam_sm_setcred, PAM_REINITIALIZE_CRED, argv_nothing,
+             "", PAM_SUCCESS,
+             "reinitialize do nothing");
+    TEST_PAM(pam_sm_setcred, PAM_REFRESH_CRED, argv_nothing,
+             "", PAM_SUCCESS,
+             "refresh do nothing");
+    TEST_PAM(pam_sm_open_session, 0, argv_nothing,
+             "", PAM_SUCCESS,
+             "open session do nothing");
+    TEST_PAM(pam_sm_close_session, 0, argv_nothing,
+             (debug ? skipping : ""), PAM_IGNORE,
+             "close session do nothing");
     pam_end(pamh, status);
 
-    /* Do nothing with debug enabled and check for correct output status. */
-    status = pam_start("test", "testuser", &conv, &pamh);
-    if (status != PAM_SUCCESS)
-        sysbail("cannot create PAM handle");
-    status = pam_sm_open_session(pamh, 0, ARRAY_SIZEOF(argv_nothing_debug) - 1,
-                                 argv_nothing_debug);
-    output = pam_output();
-    if (!k_hasafs()) {
-        is_int(status, PAM_IGNORE, "session status w/debug, no AFS");
-        asprintf(&expected, "%d pam_sm_open_session: entry (0x0)"
-                 "%d skipping, AFS apparently not available"
-                 "%d pam_sm_open_session: exit (ignore)", LOG_DEBUG,
-                 LOG_ERR, LOG_DEBUG);
-        is_string(expected, output, "session output w/debug, no AFS");
-        free(expected);
-    } else {
-        is_int(status, PAM_SUCCESS, "session status w/debug, do nothing");
-        asprintf(&expected, "%d pam_sm_open_session: entry (0x0)"
-                 "%d pam_sm_open_session: exit (success)", LOG_DEBUG,
-                 LOG_DEBUG);
-        is_string(expected, output, "session output w/debug, do nothing");
-        free(expected);
-    }
-    free(output);
-    pam_end(pamh, status);
+    free(skipping);
+}
+
+
+int
+main(void)
+{
+    plan(14 * 2);
+
+    run_tests(false);
+    run_tests(true);
 
     return 0;
 }
