a92019a45d0a95f6747e897114914f4a2fdec78a
Russ Allbery <rra@stanford.edu>
2010-12-27 08:36:03 -0800

    Refactor the PAM module test infrastructure
    Move is_pam_call and the supporting macros into a separate file so that
    other test cases can easily use them as well.

--- tests/module/basic-t.c
+++ tests/module/basic-t.c
@@ -13,85 +13,9 @@
 
 #include <syslog.h>
 
-#include <kafs/kafs.h>
 #include <tests/fakepam/testing.h>
+#include <tests/module/util.h>
 #include <tests/tap/basic.h>
-
-/*
- * These macros implement a bit of sneakiness.  For all of our argv strings,
- * debug is the last argument.  Based on the debug variable setting, choose
- * whether to include that argument or not.
- */
-#define ARRAY_SIZEOF(a) (sizeof(a) / sizeof((a)[0]))
-#define ARGV(a) ARRAY_SIZEOF(a) - (debug ? 1 : 2), (a)
-
-
-/*
- * Make a call to a PAM module function and then check it with is_pam_call.
- * Takes the PAM method, the flags, the argv to use, the expected output, the
- * expected status, and a string description.
- */
-#define TEST_PAM(m, f, a, o, s, d)                                      \
-    do {                                                                \
-        is_pam_call((o), (s), m(pamh, (f), ARGV(a)), #m, (f), debug,    \
-                    #m " %s%s", (d), debug_desc);                       \
-    } while (0)
-
-
-/*
- * Checks a PAM call, taking its return status, the expected logging output
- * and exit status, the function name, the flags passed into the call, a
- * boolean to say whether debug will be enabled (which affects the logging
- * output), and a string to use for the test description.
- */
-static void
-is_pam_call(const char *output, int expected, int seen, const char *function,
-            int flags, bool debug, const char *format, ...)
-{
-    char *logs, *desc;
-    char *p = NULL;
-    va_list args;
-
-    va_start(args, format);
-    if (vasprintf(&desc, format, args) < 0)
-        sysbail("cannot allocate memory");
-    va_end(args);
-    logs = pam_output();
-    if (!k_hasafs()) {
-        is_int(PAM_IGNORE, seen, "%s (status)", desc);
-        if (debug && strcmp(function, "pam_sm_authenticate") != 0) {
-            if (asprintf(&p, "%d %s: entry (0x%x)"
-                         "%d skipping, AFS apparently not available"
-                         "%d %s: exit (ignore)", LOG_DEBUG, function,
-                         flags, LOG_ERR, LOG_DEBUG, function) < 0)
-                sysbail("cannot allocate memory");
-        }
-        is_string(p, logs, "%s (output)", desc);
-        if (p != NULL)
-            free(p);
-    } else {
-        is_int(expected, seen, "%s (status)", desc);
-        if (debug && strcmp(function, "pam_sm_authenticate") != 0) {
-            if (asprintf(&p, "%d %s: entry (0x%x)%s%d %s: exit (%s)",
-                         LOG_DEBUG, function, flags, output, LOG_DEBUG,
-                         function, ((expected == PAM_SUCCESS) ? "success"
-                                    : (expected == PAM_IGNORE) ? "ignore"
-                                    : "failure")) < 0)
-                sysbail("cannot allocate memory");
-            is_string(p, logs, "%s (output)", desc);
-        } else {
-            if (output != NULL && output[0] == '\0')
-                is_string(NULL, logs, "%s (output)", desc);
-            else
-                is_string(output, logs, "%s (output)", desc);
-        }
-        if (p != NULL)
-            free(p);
-    }
-    if (logs != NULL)
-        free(logs);
-    free(desc);
-}
 
 
 /*
---
+++ tests/module/util.c
@@ -0,0 +1,79 @@
+/*
+ * Testing utilities for the pam-afs-sesssion module.
+ *
+ * Provides test functions used by the various test cases for the
+ * pam-afs-session module.  Normally, these are called through the macros
+ * defined in util.h.
+ *
+ * Written by Russ Allbery <rra@stanford.edu>
+ * Copyright 2010 Board of Trustees, Leland Stanford Jr. University
+ *
+ * See LICENSE for licensing terms.
+ */
+
+#include <config.h>
+#include <portable/pam.h>
+#include <portable/system.h>
+
+#include <syslog.h>
+
+#include <kafs/kafs.h>
+#include <tests/fakepam/testing.h>
+#include <tests/module/util.h>
+#include <tests/tap/basic.h>
+
+
+/*
+ * Checks a PAM call, taking its return status, the expected logging output
+ * and exit status, the function name, the flags passed into the call, a
+ * boolean to say whether debug will be enabled (which affects the logging
+ * output), and a string to use for the test description.
+ */
+void
+is_pam_call(const char *output, int expected, int seen, const char *function,
+            int flags, bool debug, const char *format, ...)
+{
+    char *logs, *desc;
+    char *p = NULL;
+    va_list args;
+
+    va_start(args, format);
+    if (vasprintf(&desc, format, args) < 0)
+        sysbail("cannot allocate memory");
+    va_end(args);
+    logs = pam_output();
+    if (!k_hasafs()) {
+        is_int(PAM_IGNORE, seen, "%s (status)", desc);
+        if (debug && strcmp(function, "pam_sm_authenticate") != 0) {
+            if (asprintf(&p, "%d %s: entry (0x%x)"
+                         "%d skipping, AFS apparently not available"
+                         "%d %s: exit (ignore)", LOG_DEBUG, function,
+                         flags, LOG_ERR, LOG_DEBUG, function) < 0)
+                sysbail("cannot allocate memory");
+        }
+        is_string(p, logs, "%s (output)", desc);
+        if (p != NULL)
+            free(p);
+    } else {
+        is_int(expected, seen, "%s (status)", desc);
+        if (debug && strcmp(function, "pam_sm_authenticate") != 0) {
+            if (asprintf(&p, "%d %s: entry (0x%x)%s%d %s: exit (%s)",
+                         LOG_DEBUG, function, flags, output, LOG_DEBUG,
+                         function, ((expected == PAM_SUCCESS) ? "success"
+                                    : (expected == PAM_IGNORE) ? "ignore"
+                                    : "failure")) < 0)
+                sysbail("cannot allocate memory");
+            is_string(p, logs, "%s (output)", desc);
+        } else {
+            if (output != NULL && output[0] == '\0')
+                is_string(NULL, logs, "%s (output)", desc);
+            else
+                is_string(output, logs, "%s (output)", desc);
+        }
+        if (p != NULL)
+            free(p);
+    }
+    if (logs != NULL)
+        free(logs);
+    free(desc);
+}
